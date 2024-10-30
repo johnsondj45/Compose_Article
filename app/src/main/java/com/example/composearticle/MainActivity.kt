f46661e10603a3a89025d33e614da2cc338bc987
Dylan Johnson <dj2330@gmail.com>
2024-10-29 20:33:14 -0400

    Finished tutorial project using Jetpack Compose

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
@@ -28,57 +28,62 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Texts(
-                        tutorialMessage = stringResource(R.string.tutorial_message),
-                        bodyOne = stringResource(R.string.body_one),
-                        bodyTwo = stringResource(R.string.body_two),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    StartingImage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Texts(
-    tutorialMessage: String,
-    bodyOne: String,
-    bodyTwo: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = tutorialMessage,
-        fontSize = 24.sp,
-        modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp)
-    )
-    Text(
-        text = bodyOne,
-        modifier = modifier.padding(start = 16.dp, end = 16.dp),
-        textAlign = TextAlign.Justify
-    )
-    Text(
-        text = bodyTwo,
-        modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
-        textAlign = TextAlign.Justify
-    )
-}
 
-@Composable
-fun headerImage(modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.bg_compose_background)
-    Image(
-        painter = image,
-        contentDescription = null,
-        modifier = modifier.fillMaxWidth()
-    )
-}
+    @Composable
+    fun Texts(
+        tutorialMessage: String,
+        bodyOne: String,
+        bodyTwo: String,
+        modifier: Modifier = Modifier
+    ) {
+        Column(modifier = modifier) {
+            Text(
+                text = tutorialMessage,
+                fontSize = 24.sp,
+                modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp)
+            )
+            Text(
+                text = bodyOne,
+                modifier = modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify
+            )
+            Text(
+                text = bodyTwo,
+                modifier = modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                ),
+                textAlign = TextAlign.Justify
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun AppPreview() {
-    ComposeArticleTheme {
-        Texts(stringResource(R.string.tutorial_message, R.string.body_one, R.string.body_two))
-        headerImage(modifier = Modifier)
+
+    }
+
+    @Composable
+    fun StartingImage(modifier: Modifier = Modifier) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Column {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = modifier.fillMaxWidth()
+            )
+            Texts(
+                tutorialMessage = stringResource(R.string.tutorial_message),
+                bodyOne = stringResource(R.string.body_one),
+                bodyTwo = stringResource(R.string.body_two)
+            )
+        }
+
     }
 }
+
