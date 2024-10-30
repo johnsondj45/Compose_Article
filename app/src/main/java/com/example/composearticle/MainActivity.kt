63874e527d3659f4d58751f1129fe31f40f83de8
Dylan Johnson <dj2330@gmail.com>
2024-10-29 20:19:33 -0400

    Added multiple text composables and image composable

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +28,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TutorialMessage(
+                    Texts(
                         tutorialMessage = stringResource(R.string.tutorial_message),
+                        bodyOne = stringResource(R.string.body_one),
+                        bodyTwo = stringResource(R.string.body_two),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,11 +41,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TutorialMessage(tutorialMessage: String, modifier: Modifier = Modifier) {
+fun Texts(
+    tutorialMessage: String,
+    bodyOne: String,
+    bodyTwo: String,
+    modifier: Modifier = Modifier
+) {
     Text(
         text = tutorialMessage,
         fontSize = 24.sp,
         modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp)
+    )
+    Text(
+        text = bodyOne,
+        modifier = modifier.padding(start = 16.dp, end = 16.dp),
+        textAlign = TextAlign.Justify
+    )
+    Text(
+        text = bodyTwo,
+        modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+        textAlign = TextAlign.Justify
+    )
+}
+
+@Composable
+fun headerImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier.fillMaxWidth()
     )
 }
 
@@ -46,6 +78,7 @@ fun TutorialMessage(tutorialMessage: String, modifier: Modifier = Modifier) {
 @Composable
 fun AppPreview() {
     ComposeArticleTheme {
-        TutorialMessage(stringResource(R.string.tutorial_message))
+        Texts(stringResource(R.string.tutorial_message, R.string.body_one, R.string.body_two))
+        headerImage(modifier = Modifier)
     }
 }
